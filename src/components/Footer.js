7aec4904e63ddaac59fa55b46ae9363bd7e3c31f
alveek <alveek@gmail.com>
2023-02-18 00:07:01 +0300

    add dynamic date in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer(props) {
   return (
     <footer className="footer">
-      <p className="footer__copyright">&copy; 2023 Mesto Russia</p>
+      <p className="footer__copyright">&copy; {new Date().getFullYear()} Mesto Russia</p>
     </footer>
   );
 }
