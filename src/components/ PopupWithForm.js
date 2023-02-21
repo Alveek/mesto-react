f613186b8bfddd71d8cc97895eb375d572a2e27f
alveek <alveek@gmail.com>
2023-02-21 22:58:08 +0300

    fixed popup animation

--- src/components/ PopupWithForm.js
+++ src/components/ PopupWithForm.js
@@ -1,15 +1,17 @@
-function PopupWithForm({title, name, isOpen, onClose, buttonText, children}) {
+function PopupWithForm({ title, name, isOpen, onClose, buttonText, children }) {
   return (
-      <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : 'popup_hidden'}`}>
-        <div className="popup__container">
-          <h2 className="popup__title">{title}</h2>
-          <form className="form" name={name} noValidate>
-            {children}
-            <button className="form__button" type="submit">{buttonText}</button>
-          </form>
-          <button onClick={onClose} type="button" className="popup__close-button opacity" aria-label="Закрыть попап"></button>
-        </div>
+    <div className={`popup popup_type_${name} ${isOpen ? 'popup_opened' : ''}`}>
+      <div className="popup__container">
+        <h2 className="popup__title">{title}</h2>
+        <form className="form" name={name} noValidate>
+          {children}
+          <button className="form__button" type="submit">
+            {buttonText}
+          </button>
+        </form>
+        <button onClick={onClose} type="button" className="popup__close-button opacity" aria-label="Закрыть попап"></button>
       </div>
+    </div>
   );
 }
 
--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import Header from './ Header';
 import Main from './ Main';
 import Footer from './Footer';
 import PopupWithForm from './ PopupWithForm';
-import {useState} from 'react';
+import { useState } from 'react';
 import ImagePopup from './ImagePopup';
 
 function App() {
@@ -21,7 +21,6 @@ function App() {
 
   const handleEditProfileClick = () => {
     setIsEditProfilePopupOpen(!isEditProfilePopupOpen);
-
   };
 
   const handleAddPlaceClick = () => {
@@ -37,83 +36,32 @@ function App() {
 
   return (
     <div className="page">
-      <Header/>
-      <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick}
-            onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick}/>
-      <Footer/>
+      <Header />
+      <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick} onCardClick={handleCardClick} />
+      <Footer />
 
-      <ImagePopup card={selectedCard} onClose={closeAllPopups}/>
+      <ImagePopup card={selectedCard} onClose={closeAllPopups} />
 
-      <PopupWithForm title="Редактировать профиль" name="profile-info"
-                     isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} buttonText="Сохранить">
-        <input
-          className="form__input form__input_user_name"
-          id="user-name-input"
-          name="name"
-          type="text"
-          placeholder="Ваше имя"
-          autoComplete="off"
-          minLength="2"
-          maxLength="40"
-          required
-        />
+      <PopupWithForm title="Редактировать профиль" name="profile-info" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups} buttonText="Сохранить">
+        <input className="form__input form__input_user_name" id="user-name-input" name="name" type="text" placeholder="Ваше имя" autoComplete="off" minLength="2" maxLength="40" required />
         <span className="form__input-error user-name-input-error"></span>
-        <input
-          className="form__input form__input_user_job"
-          id="user-job-input"
-          name="about"
-          type="text"
-          placeholder="Род деятельности"
-          autoComplete="off"
-          minLength="2"
-          maxLength="200"
-          required
-        />
+        <input className="form__input form__input_user_job" id="user-job-input" name="about" type="text" placeholder="Род деятельности" autoComplete="off" minLength="2" maxLength="200" required />
         <span className="form__input-error user-job-input-error"></span>
       </PopupWithForm>
 
-      <PopupWithForm title="Новое место" name="add-card" isOpen={isAddPlacePopupOpen}
-                     onClose={closeAllPopups} buttonText="Создать">
-        <input
-          className="form__input form__input_card_name"
-          id="card-name-input"
-          name="cardName"
-          type="text"
-          placeholder="Название"
-          autoComplete="off"
-          minLength="2"
-          maxLength="40"
-          required
-        />
+      <PopupWithForm title="Новое место" name="add-card" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups} buttonText="Создать">
+        <input className="form__input form__input_card_name" id="card-name-input" name="cardName" type="text" placeholder="Название" autoComplete="off" minLength="2" maxLength="40" required />
         <span className="form__input-error card-name-input-error"></span>
-        <input
-          className="form__input form__input_card_link"
-          id="card-link-input"
-          name="cardLink"
-          type="url"
-          placeholder="Ссылка на картинку"
-          autoComplete="off"
-          required
-        />
+        <input className="form__input form__input_card_link" id="card-link-input" name="cardLink" type="url" placeholder="Ссылка на картинку" autoComplete="off" required />
         <span className="form__input-error card-link-input-error"></span>
       </PopupWithForm>
 
-      <PopupWithForm title="Обновить аватар" name="update-avatar"
-                     isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} buttonText="Сохранить">
-        <input
-          className="form__input form__input_avatar_link"
-          id="avatar-link-input"
-          name="avatarLink"
-          type="url"
-          placeholder="Ссылка на картинку"
-          autoComplete="off"
-          required
-        />
+      <PopupWithForm title="Обновить аватар" name="update-avatar" isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups} buttonText="Сохранить">
+        <input className="form__input form__input_avatar_link" id="avatar-link-input" name="avatarLink" type="url" placeholder="Ссылка на картинку" autoComplete="off" required />
         <span className="form__input-error avatar-link-input-error"></span>
       </PopupWithForm>
 
-      <PopupWithForm title="Вы уверены?" name="delete-card" buttonText="Да">
-      </PopupWithForm>
+      <PopupWithForm title="Вы уверены?" name="delete-card" buttonText="Да"></PopupWithForm>
     </div>
   );
 }
